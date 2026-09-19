cc8038b007e22ba0516d1b6285f8e92bb0518bbe
Deep-sabhaya2007 <deepsabhay38@gmail.com>
2026-09-19 23:24:36 +0530

    Add input handling and loop to print numbers in lab1.c

--- C/lab1.c
+++ C/lab1.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 void main()
 {
+    int n;
+    printf("enter the value of n : ");
+    scanf("%d",&n);
+    
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d \n",i);
+    }
     
 }
