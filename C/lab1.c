6d7bea215e0e9b62a8000de9cbb05889ceaf3f18
Deep-sabhaya2007 <deepsabhay38@gmail.com>
2026-09-19 17:40:22 +0530

    Refactor grading logic and add input validation in lab1.c; create new executable a.exe

--- C/lab1.c
+++ C/lab1.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 void main()
 {
-    int h, b;
-    float area;
-    printf("Enter the value of h : ");
-    scanf("%d", &h);
+    int mark;
+    printf("Enter your mark: ");
+    scanf("%d", &mark);
 
-    printf("Enter the value of b : ");
-    scanf("%d", &b);
-
-    area = 0.5 * h * b;
-    printf("Area of triangle = %f", area);
+    if (mark > 90 && mark <= 100)
+    {
+        printf("grade A");
+    }
+    else if (mark > 80 && mark <= 90)
+    {
+        printf("grade B");
+    }
+    else if (mark > 70 && mark <= 80)
+    {
+        printf("gade c");
+    }
+    else if (mark >60 && mark <=70)
+    {
+        printf("grade D");
+    }
+    else if(mark >50 && mark <= 60)
+    {
+        printf("grade E");
+    }
+    else if(mark >= 0 && mark <= 50)
+    {
+        printf("grade F");
+    }
+    else
+    {
+        printf("Invalid mark");
+    }
 }
